4e61db4c9537235eefee61442a248a9f51d41e66
Tomas Stropus <h.guzas@gmail.com>
2023-05-20 23:58:06 +0300

    Add graceful handling of cases when there are RPC call to transmission fails

--- Transmission.cs
+++ Transmission.cs
@@ -1,46 +1,55 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
-using System.Linq;
-using Newtonsoft.Json;
 using Transmission.API.RPC;
 using System.Collections.Generic;
 using System.Threading;
+using Transmission.API.RPC.Entity;
 
 namespace transmission_watcher
 {
     public class TransmissionWatcher
     {
-        private string[] fields = new string[] { "id", "isPrivate", "name", "status", "uploadRatio", "downloadDir", "peersConnected" };
+        private readonly string[] _fields = { "id", "isPrivate", "name", "status", "uploadRatio", "downloadDir", "peersConnected" };
 
-
-        private Client TransmissionClient = null;
+        private Client _transmissionClient;
 
         public async Task Start()
         {
             Console.WriteLine("Starting Transmission Watcher...");
             var transmissionPath = Environment.GetEnvironmentVariable("TRANSMISSION_PATH");
             var url = $"http://{transmissionPath}";
-            this.TransmissionClient = new Client(url);
+            _transmissionClient = new Client(url);
             await CheckTorrents();
         }
 
-        public async Task CheckTorrents()
+        private async Task CheckTorrents()
         {
             var timer = new PeriodicTimer(TimeSpan.FromSeconds(60));
 
             while (await timer.WaitForNextTickAsync())
             {
-                var torrents = await this.TransmissionClient.TorrentGetAsync(fields);
+                TransmissionTorrents torrents;
+                
+                try
+                {
+                    torrents = await _transmissionClient.TorrentGetAsync(_fields);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to fetch torrents!");
+
+                    continue;
+                }
                 Console.WriteLine($"Total Torrents: {torrents.Torrents.Length}");
                 var toStopList = new List<object>();
-                Double.TryParse(Environment.GetEnvironmentVariable("STOP_RATIO"), out double stopRatio);
-                Boolean.TryParse(Environment.GetEnvironmentVariable("HELP_SOLO_PEERS"), out bool helpSoloPeers);
+                double.TryParse(Environment.GetEnvironmentVariable("STOP_RATIO"), out var stopRatio);
+                bool.TryParse(Environment.GetEnvironmentVariable("HELP_SOLO_PEERS"), out var helpSoloPeers);
                 foreach (var torrent in torrents.Torrents)
                 {
                     if (torrent.Status == 6) //seeding
                     {
                         Console.WriteLine($"{torrent.Name} is finished, it's seed ratio is {torrent.uploadRatio}");
+                        
                         if (torrent.uploadRatio >= stopRatio && (!helpSoloPeers || torrent.PeersConnected > 2 || torrent.PeersConnected == 0))
                         {
                             Console.WriteLine($"Goodbye forever, {torrent.Name}. You won't be missed.");
@@ -51,8 +60,9 @@ namespace transmission_watcher
                 if (toStopList.Count > 0)
                 {
                     Console.WriteLine($"Stopping {toStopList.Count} torrents.");
-                    this.TransmissionClient.TorrentStopAsync(toStopList.ToArray());
+                    _transmissionClient.TorrentStopAsync(toStopList.ToArray());
                 }
+                
                 Console.WriteLine("Sleeping for 60 seconds.");
             }
         }
